64830217b93fdddae431d5d96449ad2b7e1d4fc0
williamtesche <85793944+FelipeSimoesDaRocha@users.noreply.github.com>
2021-08-31 09:33:57 -0300

    Update

--- Listar Itens js/app.js
+++ Listar Itens js/app.js
@@ -1,2 +1,33 @@
-const todoInput = document.querySelector('.int')
-console.log(int)
+const todoInput = document.querySelector(".todo-input");
+
+const todoButton = document.querySelector(".todo-button");
+
+const todoList = document.querySelector(".todo-list");
+console.log(todoButton);
+
+todoButton.addEventListener("click", addTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  const todoLi = document.createElement("li");
+  todoLi.classList.add("todo-list");
+  todoLi.innerText = "sou foda";
+
+  todoDiv.appendChildal(todoLi);
+
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("completed-btn");
+  todoDiv.appendChild(completedButton);
+
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  todoList.appendChild(todoDiv);
+}
